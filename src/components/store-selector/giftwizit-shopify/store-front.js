1c5701b3c190b2681ac13412853e97cff3d56510
Brian West <b.sanders7777@edmail.edcc.edu>
2019-10-05 00:25:17 -0700

    Added continuous load scrolling
    Added continuous load scrolling to store-front

--- src/components/store-selector/giftwizit-shopify/store-front.js
+++ src/components/store-selector/giftwizit-shopify/store-front.js
@@ -104,6 +104,12 @@ class StoreFront extends Component {
     productSelected = (product) => {
         this.props.navigation.navigate("Products", {productId: product.id});
     }
+    fetchNextPageOfProducts = () => {
+        let lastProductIndex = this.props.products.length - 1;
+        if(this.props.products[lastProductIndex].hasNextPage) {
+            this.props.fetchNextPage();
+        }
+    }
     render() {
         if(this.props.activeCategory != null && this.props.products.length == 0) {
             this.fetchCategoryProducts();
@@ -113,11 +119,13 @@ class StoreFront extends Component {
                 horizontal={false}
                 data={this.props.products}
                 extraData={this.props.wishList}
+                onEndReached={this.fetchNextPageOfProducts}
+                onEndReachedThreshold={1}
                 renderItem={(product) => {
                     const images = product.item.images;
                     return product.item.availableForSale == true 
                         ?
-                            <Card containerStyle={{minHeight: 100}}>
+                            <Card key={product.item.id} containerStyle={{minHeight: 100}}>
                                 <TouchableOpacity onPress={() => this.productSelected(product.item)}>
                                     <View style={{flexDirection: 'row', flex: 2, maxHeight: 55}}>
                                         <View style={{flex: 1}}>
@@ -190,7 +198,8 @@ mapDispatchToProps = dispatch => {
         fetchCategoryProducts: () => dispatch(actions.fetchCategoryProducts()),
         addItemToCart: (lineItems) => dispatch(actions.addItemToCart(lineItems)),
         addItemToWList: (item) => dispatch(actions.addWishListItem(item)),
-        removeItemFromWList: (item) => dispatch(actions.deleteWishListItems(item))
+        removeItemFromWList: (item) => dispatch(actions.deleteWishListItems(item)),
+        fetchNextPage: () => dispatch(actions.fetchNextPageOfProducts())
     }
 }
 
--- src/store/reducers/storefront-reducer.js
+++ src/store/reducers/storefront-reducer.js
@@ -51,6 +51,11 @@ const storeFrontReducer = (state = initialState, action) => {
                 ...state,
                 activeProduct: null
             }
+        case actionTypes.GET_PRODUCTS_NEXT_PAGE:
+            return {
+                ...state,
+                displayedProducts: state.displayedProducts.concat(action.payload)
+            }
         default: return state;
     }
 }
