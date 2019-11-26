a5cb71cb7852de9d1b8042e12fda1251cf77729d
Brian West <brianwest@Brians-MacBook-Pro.local>
2019-11-26 00:20:14 -0800

    Attempting to implement new stack modal

---
+++ src/components/navigation/modal-screens/gift-list-chat-modal.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { Text } from 'react-native'
+
+class GiftListChatModal extends Component {
+    render(){
+        return (
+            <Text>Gift List Chat Modal!</Text>
+        )
+    }
+}
+
+export default GiftListChatModal;
---
+++ src/components/navigation/modal-screens/index.js
@@ -0,0 +1,5 @@
+export {
+    GiftListChatModal
+} from './gift-list-chat-modal';
+
+export { GiftListsModal } from './gift-lists-modal';
